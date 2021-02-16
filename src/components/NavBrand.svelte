845606215140e2301fdfb69d5de63e49f418ebb7
Nirjan Khadka <webdev.nk@gmail.com>
2021-02-16 08:34:53 +0545

    feat: :sparkles: use new logo and favicon

--- src/components/NavBrand.svelte
+++ src/components/NavBrand.svelte
@@ -11,9 +11,11 @@
     margin: 0.2rem;
     max-width: 100%;
     height: auto;
-    padding: var(--spacing-0);
+    padding: var(--spacing-1);
+    max-height: 4.5rem;
     @media (max-width: 40em) {
-      padding: 0;
+      max-height: 3.5rem;
+      padding: var(--spacing-0);
     }
   }
 </style>
--- src/layouts/Layout.svelte
+++ src/layouts/Layout.svelte
@@ -9,6 +9,13 @@
 </style>
 
 <svelte:head>
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+  <link rel="manifest" href="/site.webmanifest" />
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#845ec2" />
+  <meta name="msapplication-TileColor" content="#fbeaff" />
+  <meta name="theme-color" content="#fbeaff" />
   <script defer src="/lazysizes/lazysizes.min.js">
   </script>
   <script defer src="/lazysizes/ls.blur-up.min.js">
